5afa16a3e450121b0e9b1af051a0ad7412004f8f
ikhsannn <55969166+ikhsannn@users.noreply.github.com>
2019-10-08 18:50:40 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.3.75/accounting/';
+$config['base_url'] = 'https://accounting-codeigniter-materialize.github.io';
 
 /*
 |--------------------------------------------------------------------------
